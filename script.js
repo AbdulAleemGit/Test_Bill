d24780460f470a4ad2ea84bb0edf03d7b6cd2597
AbdulAleemGit <abdul.aleem@outlook.com>
2021-03-01 00:38:30 -0600

    first commit

--- script.js
+++ script.js
@@ -1,238 +1,252 @@
-/* Shivving (IE8 is not supported, but at least it won't look as awful)
-/* ========================================================================== */
+  /* Shivving (IE8 is not supported, but at least it won't look as awful)
+  /* ========================================================================== */
 
-(function (document) {
-	var
-	head = document.head = document.getElementsByTagName('head')[0] || document.documentElement,
-	elements = 'article aside audio bdi canvas data datalist details figcaption figure footer header hgroup mark meter nav output picture progress section summary time video x'.split(' '),
-	elementsLength = elements.length,
-	elementsIndex = 0,
-	element;
+  (function (document) {
+    var
+    head = document.head = document.getElementsByTagName('head')[0] || document.documentElement,
+    elements = 'article aside audio bdi canvas data datalist details figcaption figure footer header hgroup mark meter nav output picture progress section summary time video x'.split(' '),
+    elementsLength = elements.length,
+    elementsIndex = 0,
+    element;
 
-	while (elementsIndex < elementsLength) {
-		element = document.createElement(elements[++elementsIndex]);
-	}
+    while (elementsIndex < elementsLength) {
+      element = document.createElement(elements[++elementsIndex]);
+    }
 
-	element.innerHTML = 'x<style>' +
-		'article,aside,details,figcaption,figure,footer,header,hgroup,nav,section{display:block}' +
-		'audio[controls],canvas,video{display:inline-block}' +
-		'[hidden],audio{display:none}' +
-		'mark{background:#FF0;color:#000}' +
-	'</style>';
+    element.innerHTML = 'x<style>' +
+      'article,aside,details,figcaption,figure,footer,header,hgroup,nav,section{display:block}' +
+      'audio[controls],canvas,video{display:inline-block}' +
+      '[hidden],audio{display:none}' +
+      'mark{background:#FF0;color:#000}' +
+    '</style>';
 
-	return head.insertBefore(element.lastChild, head.firstChild);
-})(document);
+    return head.insertBefore(element.lastChild, head.firstChild);
+  })(document);
 
-/* Prototyping
-/* ========================================================================== */
+  /* Prototyping
+  /* ========================================================================== */
 
-(function (window, ElementPrototype, ArrayPrototype, polyfill) {
-	function NodeList() { [polyfill] }
-	NodeList.prototype.length = ArrayPrototype.length;
+  (function (window, ElementPrototype, ArrayPrototype, polyfill) {
+    function NodeList() { [polyfill] }
+    NodeList.prototype.length = ArrayPrototype.length;
 
-	ElementPrototype.matchesSelector = ElementPrototype.matchesSelector ||
-	ElementPrototype.mozMatchesSelector ||
-	ElementPrototype.msMatchesSelector ||
-	ElementPrototype.oMatchesSelector ||
-	ElementPrototype.webkitMatchesSelector ||
-	function matchesSelector(selector) {
-		return ArrayPrototype.indexOf.call(this.parentNode.querySelectorAll(selector), this) > -1;
-	};
+    ElementPrototype.matchesSelector = ElementPrototype.matchesSelector ||
+    ElementPrototype.mozMatchesSelector ||
+    ElementPrototype.msMatchesSelector ||
+    ElementPrototype.oMatchesSelector ||
+    ElementPrototype.webkitMatchesSelector ||
+    function matchesSelector(selector) {
+      return ArrayPrototype.indexOf.call(this.parentNode.querySelectorAll(selector), this) > -1;
+    };
 
-	ElementPrototype.ancestorQuerySelectorAll = ElementPrototype.ancestorQuerySelectorAll ||
-	ElementPrototype.mozAncestorQuerySelectorAll ||
-	ElementPrototype.msAncestorQuerySelectorAll ||
-	ElementPrototype.oAncestorQuerySelectorAll ||
-	ElementPrototype.webkitAncestorQuerySelectorAll ||
-	function ancestorQuerySelectorAll(selector) {
-		for (var cite = this, newNodeList = new NodeList; cite = cite.parentElement;) {
-			if (cite.matchesSelector(selector)) ArrayPrototype.push.call(newNodeList, cite);
-		}
+    ElementPrototype.ancestorQuerySelectorAll = ElementPrototype.ancestorQuerySelectorAll ||
+    ElementPrototype.mozAncestorQuerySelectorAll ||
+    ElementPrototype.msAncestorQuerySelectorAll ||
+    ElementPrototype.oAncestorQuerySelectorAll ||
+    ElementPrototype.webkitAncestorQuerySelectorAll ||
+    function ancestorQuerySelectorAll(selector) {
+      for (var cite = this, newNodeList = new NodeList; cite = cite.parentElement;) {
+        if (cite.matchesSelector(selector)) ArrayPrototype.push.call(newNodeList, cite);
+      }
 
-		return newNodeList;
-	};
+      return newNodeList;
+    };
 
-	ElementPrototype.ancestorQuerySelector = ElementPrototype.ancestorQuerySelector ||
-	ElementPrototype.mozAncestorQuerySelector ||
-	ElementPrototype.msAncestorQuerySelector ||
-	ElementPrototype.oAncestorQuerySelector ||
-	ElementPrototype.webkitAncestorQuerySelector ||
-	function ancestorQuerySelector(selector) {
-		return this.ancestorQuerySelectorAll(selector)[0] || null;
-	};
-})(this, Element.prototype, Array.prototype);
+    ElementPrototype.ancestorQuerySelector = ElementPrototype.ancestorQuerySelector ||
+    ElementPrototype.mozAncestorQuerySelector ||
+    ElementPrototype.msAncestorQuerySelector ||
+    ElementPrototype.oAncestorQuerySelector ||
+    ElementPrototype.webkitAncestorQuerySelector ||
+    function ancestorQuerySelector(selector) {
+      return this.ancestorQuerySelectorAll(selector)[0] || null;
+    };
+  })(this, Element.prototype, Array.prototype);
 
-/* Helper Functions
-/* ========================================================================== */
+  /* Helper Functions
+  /* ========================================================================== */
 
-/* function generateTableRow() {
-	var emptyColumn = document.createElement('tr');
+  /* function generateTableRow() {
+    var emptyColumn = document.createElement('tr');
 
-	emptyColumn.innerHTML = '<td><a class="cut">-</a><span contenteditable></span></td>' +
-		'<td><span contenteditable></span></td>' +
-		'<td><span data-prefix>$</span><span contenteditable>0.00</span></td>' +
-		'<td><span contenteditable>0</span></td>' +
-		'<td><span data-prefix>$</span><span>0.00</span></td>';
+    emptyColumn.innerHTML = '<td><a class="cut">-</a><span contenteditable></span></td>' +
+      '<td><span contenteditable></span></td>' +
+      '<td><span data-prefix>$</span><span contenteditable>0.00</span></td>' +
+      '<td><span contenteditable>0</span></td>' +
+      '<td><span data-prefix>$</span><span>0.00</span></td>';
 
-	return emptyColumn;
-}
-*/
-function parseFloatHTML(element) {
-	return parseFloat(element.innerHTML.replace(/[^\d\.\-]+/g, '')) || 0;
-}
+    return emptyColumn;
+  }
+  */
+  function parseFloatHTML(element) {
+    return parseFloat(element.innerHTML.replace(/[^\d\.\-]+/g, '')) || 0;
+  }
 
-function parsePrice(number) {
-	return number.toFixed(2).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1,');
-}
+  function parsePrice(number) {
+    return number.toFixed(2).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1,');
+  }
 
-/* Update Number
-/* ========================================================================== */
+  /* Update Number
+  /* ========================================================================== */
 
-function updateNumber(e) {
-	var
-	activeElement = document.activeElement,
-	value = parseFloat(activeElement.innerHTML),
-	wasPrice = activeElement.innerHTML == parsePrice(parseFloatHTML(activeElement));
+  function updateNumber(e) {
+    var
+    activeElement = document.activeElement,
+    value = parseFloat(activeElement.innerHTML),
+    wasPrice = activeElement.innerHTML == parsePrice(parseFloatHTML(activeElement));
 
-	if (!isNaN(value) && (e.keyCode == 38 || e.keyCode == 40 || e.wheelDeltaY)) {
-		e.preventDefault();
+    if (!isNaN(value) && (e.keyCode == 38 || e.keyCode == 40 || e.wheelDeltaY)) {
+      e.preventDefault();
 
-		value += e.keyCode == 38 ? 1 : e.keyCode == 40 ? -1 : Math.round(e.wheelDelta * 0.025);
-		value = Math.max(value, 0);
+      value += e.keyCode == 38 ? 1 : e.keyCode == 40 ? -1 : Math.round(e.wheelDelta * 0.025);
+      value = Math.max(value, 0);
 
-		activeElement.innerHTML = wasPrice ? parsePrice(value) : value;
-	}
+      activeElement.innerHTML = wasPrice ? parsePrice(value) : value;
+    }
 
-	updateInvoice();
-}
+    updateInvoice();
+  }
 
-/* Update Invoice
-/* ========================================================================== */
+  /* Update Invoice
+  /* ========================================================================== */
 
-function updateInvoice() {
-	var total = 0;
-	var cells, price, total, a, i;
+  function updateInvoice() {
+    var total = 0;
+    var tips = 0;
+    var cells, price, total, a, i, tips,drinks;
 
-	// update inventory cells
-	// ======================
+    // update inventory cells
+    // ======================
 
-	for (var a = document.querySelectorAll('table.inventory tbody tr'), i = 0; a[i]; ++i) {
-		// get inventory row cells
-		cells = a[i].querySelectorAll('span:last-child');
+    for (var a = document.querySelectorAll('table.inventory tbody tr'), i = 0; a[i]; ++i) {
+      // get inventory row cells
+      cells = a[i].querySelectorAll('span:last-child');
 
-		// set price as cell[2] * cell[3]
-		price = parseFloatHTML(cells[2]) * parseFloatHTML(cells[3]);
+      // set price as cell[2] * cell[3]
+      price = parseFloatHTML(cells[2]) * parseFloatHTML(cells[3]);
 
-		// add price to total
-		total += price;
+      // add price to total
+      total += price;
 
-		// set row total
-		cells[4].innerHTML = price;
-	}
+      //set tips 
+      if (!(cells[0].innerHTML==='Cola') && !(cells[0].innerHTML==='Coffee'))
+        {
+          tips += price * .15;
+        }
+	  //drinks += tips
+      // set row total
+      cells[4].innerHTML = price;
 
-	// update balance cells
-	// ====================
+      //set tips
 
-	// get balance cells
-	cells = document.querySelectorAll('table.balance td:last-child span:last-child');
 
-	// set total
-	cells[0].innerHTML = total;
+    }
 
-	// set balance and meta balance
-	cells[2].innerHTML = document.querySelector('table.meta tr:last-child td:last-child span:last-child').innerHTML = parsePrice(total - parseFloatHTML(cells[1]));
+    // update balance cells
+    // ====================
 
-	// update prefix formatting
-	// ========================
+    // get balance cells
+    cells = document.querySelectorAll('table.balance td:last-child span:last-child');
 
-	var prefix = document.querySelector('#prefix').innerHTML;
-	for (a = document.querySelectorAll('[data-prefix]'), i = 0; a[i]; ++i) a[i].innerHTML = prefix;
+    // set total
+    cells[0].innerHTML = total;
 
-	// update price formatting
-	// =======================
+    //set Amount paid
+    cells[1].innerHTML = tips;
 
-	for (a = document.querySelectorAll('span[data-prefix] + span'), i = 0; a[i]; ++i) if (document.activeElement != a[i]) a[i].innerHTML = parsePrice(parseFloatHTML(a[i]));
-}
+    // set balance and meta balance
+    cells[2].innerHTML = document.querySelector('table.meta tr:last-child td:last-child span:last-child').innerHTML = parsePrice(total + parseFloatHTML(cells[1]));
 
-/* On Content Load
-/* ========================================================================== */
+    // update prefix formatting
+    // ========================
 
-function onContentLoad() {
-	updateInvoice();
+    var prefix = document.querySelector('#prefix').innerHTML;
+    for (a = document.querySelectorAll('[data-prefix]'), i = 0; a[i]; ++i) a[i].innerHTML = prefix;
 
-	var
-	input = document.querySelector('input'),
-	image = document.querySelector('img');
+    // update price formatting
+    // =======================
 
-	function onClick(e) {
-		var element = e.target.querySelector('[contenteditable]'), row;
+    for (a = document.querySelectorAll('span[data-prefix] + span'), i = 0; a[i]; ++i) if (document.activeElement != a[i]) a[i].innerHTML = parsePrice(parseFloatHTML(a[i]));
+  }
 
-		element && e.target != document.documentElement && e.target != document.body && element.focus();
+  /* On Content Load
+  /* ========================================================================== */
 
-		if (e.target.matchesSelector('.add')) {
-			document.querySelector('table.inventory tbody').appendChild(generateTableRow());
-		}
-		else if (e.target.className == 'cut') {
-			row = e.target.ancestorQuerySelector('tr');
+  function onContentLoad() {
+    updateInvoice();
 
-			row.parentNode.removeChild(row);
-		}
+    var
+    input = document.querySelector('input'),
+    image = document.querySelector('img');
 
-		updateInvoice();
-	}
+    function onClick(e) {
+      var element = e.target.querySelector('[contenteditable]'), row;
 
-	function onEnterCancel(e) {
-		e.preventDefault();
+      element && e.target != document.documentElement && e.target != document.body && element.focus();
 
-		image.classList.add('hover');
-	}
+      if (e.target.matchesSelector('.add')) {
+        document.querySelector('table.inventory tbody').appendChild(generateTableRow());
+      }
+      else if (e.target.className == 'cut') {
+        row = e.target.ancestorQuerySelector('tr');
 
-	function onLeaveCancel(e) {
-		e.preventDefault();
+        row.parentNode.removeChild(row);
+      }
 
-		image.classList.remove('hover');
-	}
+      updateInvoice();
+    }
 
-	function onFileInput(e) {
-		image.classList.remove('hover');
+    function onEnterCancel(e) {
+      e.preventDefault();
 
-		var
-		reader = new FileReader(),
-		files = e.dataTransfer ? e.dataTransfer.files : e.target.files,
-		i = 0;
+      image.classList.add('hover');
+    }
 
-		reader.onload = onFileLoad;
+    function onLeaveCancel(e) {
+      e.preventDefault();
 
-		while (files[i]) reader.readAsDataURL(files[i++]);
-	}
+      image.classList.remove('hover');
+    }
 
-	function onFileLoad(e) {
-		var data = e.target.result;
+    function onFileInput(e) {
+      image.classList.remove('hover');
 
-		image.src = data;
-	}
+      var
+      reader = new FileReader(),
+      files = e.dataTransfer ? e.dataTransfer.files : e.target.files,
+      i = 0;
 
-	if (window.addEventListener) {
-		document.addEventListener('click', onClick);
+      reader.onload = onFileLoad;
 
-		document.addEventListener('mousewheel', updateNumber);
-		document.addEventListener('keydown', updateNumber);
+      while (files[i]) reader.readAsDataURL(files[i++]);
+    }
 
-		document.addEventListener('keydown', updateInvoice);
-		document.addEventListener('keyup', updateInvoice);
+    function onFileLoad(e) {
+      var data = e.target.result;
 
-		input.addEventListener('focus', onEnterCancel);
-		input.addEventListener('mouseover', onEnterCancel);
-		input.addEventListener('dragover', onEnterCancel);
-		input.addEventListener('dragenter', onEnterCancel);
+      image.src = data;
+    }
 
-		input.addEventListener('blur', onLeaveCancel);
-		input.addEventListener('dragleave', onLeaveCancel);
-		input.addEventListener('mouseout', onLeaveCancel);
+    if (window.addEventListener) {
+      document.addEventListener('click', onClick);
 
-		input.addEventListener('drop', onFileInput);
-		input.addEventListener('change', onFileInput);
-	}
-}
+      document.addEventListener('mousewheel', updateNumber);
+      document.addEventListener('keydown', updateNumber);
 
-window.addEventListener && document.addEventListener('DOMContentLoaded', onContentLoad);
+      document.addEventListener('keydown', updateInvoice);
+      document.addEventListener('keyup', updateInvoice);
+
+      input.addEventListener('focus', onEnterCancel);
+      input.addEventListener('mouseover', onEnterCancel);
+      input.addEventListener('dragover', onEnterCancel);
+      input.addEventListener('dragenter', onEnterCancel);
+
+      input.addEventListener('blur', onLeaveCancel);
+      input.addEventListener('dragleave', onLeaveCancel);
+      input.addEventListener('mouseout', onLeaveCancel);
+
+      input.addEventListener('drop', onFileInput);
+      input.addEventListener('change', onFileInput);
+    }
+  }
+
+  window.addEventListener && document.addEventListener('DOMContentLoaded', onContentLoad);
